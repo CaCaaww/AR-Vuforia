ddab3e9a57bba0056beedc8b84d32594473f258d
BirdTheProgrammer <JamesAllenHansplant@gmail.com>
2024-05-28 16:36:28 +0200

    Ryan and James
    Finished the Curtain. All is good. Will comment later 5-28-24

--- Assets/AR-Project/Scripts/UI/Login/CurtainScript.cs
+++ Assets/AR-Project/Scripts/UI/Login/CurtainScript.cs
@@ -14,16 +14,17 @@ public class CurtainScript : MonoBehaviour {
     public float curtainTime = .25f;
     private bool curtainOnScreen = false;
     private float length;
-    private void Awake() {
+    private void Start() {
         length = curtainRectTransform.rect.height;
         if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("01-LoginScene")) {
             curtainOnScreen = false;
         } else {
+            curtainRectTransform.anchoredPosition = Vector2.zero;
             curtainOnScreen = true;
-            toggleCurtain();
+            Invoke("toggleCurtain", 1);
         }
     }
-    
+
     public void toggleCurtain() {
         if (curtainOnScreen) {
             curtainUp();
@@ -34,7 +35,8 @@ public class CurtainScript : MonoBehaviour {
         //Debug.Log("TOGGLING CURTAIN ");
     }
     private void curtainUp() {
-        curtainRectTransform.DOAnchorPos(new Vector2(0, length), curtainTime, false);
+        //Debug.Log("||||||| Y Position: " + curtainRectTransform.anchoredPosition.y);
+        curtainRectTransform.DOAnchorPos(new Vector2(0, length), curtainTime, false).SetEase(Ease.InSine);
     }
     private void curtainDown() { 
         curtainRectTransform.DOAnchorPos(Vector2.zero, curtainTime, false);
